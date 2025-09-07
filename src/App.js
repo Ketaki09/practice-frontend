5c56e9eaeb65a48ca7e8323ed57e9f808ed68840
Ketaki Shintre <44314233+Ketaki09@users.noreply.github.com>
2025-09-07 15:40:16 -0700

    router browser

--- src/App.js
+++ src/App.js
@@ -2,18 +2,16 @@ import React from "react";
 import ReactDom from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-
+import { Outlet } from "react-router-dom";
 
 const AppLayout = () => {
   return (
     <div className="app">
       <Header/>
-      <Body/>
+      <Outlet/>
     </div>
   );
 }
 
-// const root = ReactDom.createRoot(document.getElementById("root"));
-// root.render(<AppLayout />);
 
 export default AppLayout;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import { CDN_URL } from "../utils/constants";
 import "../index.css"
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 const Header = () => {
   const [btnState, setBtnState] = useState("login");
   console.log("btnState", btnState);
@@ -10,10 +11,10 @@ const Header = () => {
         <img className="logo" src={CDN_URL}/>
       </div>
       <div className="nav-items">
-        <ul>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Contact Us</li>
+        <ul> 
+          <li><Link to ="/about">About Us</Link></li>
+          <li><Link to ="/">Home</Link></li>
+          <li><Link to ="/contact">Contact Us</Link></li>
           <li>Cart</li>
           <button className="login" onClick={()=> {btnState === "login"?setBtnState("logout"):setBtnState("login")}}>{btnState}</button>
         </ul>
